db1c033f0281aee933d0a25ef8fcb4a8620ee4cd
John Ritsema <john.ritsema@turner.com>
2020-02-28 15:26:28 -0500

    updates home dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 # create config file
-echo '{"apiKey":"'"$API_KEY"'","apiEndpoint":"https://'$INPUT_ENVIRONMENT'.api.contentai.io"}' > ~/.contentai/config
+echo '{"apiKey":"'"$API_KEY"'","apiEndpoint":"https://'$INPUT_ENVIRONMENT'.api.contentai.io"}' > /github/home/.contentai/config
 
 # execute contentai command with args
 sh -c "./contentai $*"
