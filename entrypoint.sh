7e2fac0c2f95cfd5b2c3044c235bdd553089ef57
John Ritsema <john.ritsema@turner.com>
2020-02-28 14:42:31 -0500

    config file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,8 @@
 #!/bin/sh -l
+
+# create config file
+echo '{"apiKey":"'"$API_KEY"'","apiEndpoint":"https://"'$1'".api.contentai.io"}' > ~/.contentai/config
+cat ~/.contentai/config
+
+# execute contentai command with args
 sh -c contentai "$*"
